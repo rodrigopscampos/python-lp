d721c9ff9a83b583da124ed370f5e0dda049d6d1
Rodrigo Campos <rodigopscampos@gmail.com>
2019-08-01 18:28:24 -0300

    adiciona exemplo de processamento de csv

--- io/extra.py
+++ io/extra.py
@@ -1,6 +1,25 @@
-f = open('arquivo.csv', 'r')
+# A ultima coluna do arquivo.csv (Selecione Tipo de CE Simp 4)
+# possui dois números separados por vírgula
+# O desafio é fazer um script que gera outro csv com os 
+# dois valores separados em colunas diferentes
 
-linhas = list(f)
+origem = open('arquivo.csv', 'r')
+destino = open('destino.csv', 'a')
+
+linhas = list(origem)
+cabecalho = linhas[0].strip() + ";Coluna2\n"
+destino.write(cabecalho)
 
 for linha in linhas[1:]:
-    print(linha.strip())
+    colunas = linha.split(';')
+    ultima_coluna = colunas[-1]
+    del colunas[-1]
+
+    valores = ultima_coluna.split(',')
+    colunas = colunas + valores
+    destino.write( ";".join(colunas).strip() + "\n")
+
+origem.close()
+destino.close()
+
+print("fim")
